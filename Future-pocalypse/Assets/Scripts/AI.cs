649671491ad709f05a17019d828af15a1733f3bc
Crashex <32877996+Crashex@users.noreply.github.com>
2018-02-14 22:17:20 +0000

    AI update. GUI update.
    GUI updated, now opening the V menu opens a group of buttons not just one button.
    
    AI changed from update to fixed update.

--- Future-pocalypse/Assets/Scripts/AI.cs
+++ Future-pocalypse/Assets/Scripts/AI.cs
@@ -62,7 +62,7 @@ public class AI : MonoBehaviour
         }
     }
 
-    void Update()
+    void FixedUpdate()
     {
 
         if (Physics.Raycast(transform.position, -transform.up, out hit, bc.size.y/2))
@@ -169,9 +169,9 @@ public class AI : MonoBehaviour
 
 
                 //rb.MovePosition(transform.position + transform.forward * vehicleSpeed * Time.deltaTime);
-                if (rb.velocity.magnitude <  maxspeed && onfloor == true)
+                if (rb.velocity.magnitude <=  maxspeed + difficulty && onfloor == true)
                 {
-                    rb.AddForce(transform.forward * (vehicleSpeed + difficulty)/Time.deltaTime , ForceMode.Acceleration);
+                    rb.AddForce(transform.forward * vehicleSpeed  , ForceMode.Acceleration);
                 }
             }
         }
