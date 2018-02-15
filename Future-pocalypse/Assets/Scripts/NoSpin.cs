c2c2e1d499a7292e962eb46c89c8194265f251df
GinnSan98 <32876454+GinnSan98@users.noreply.github.com>
2018-02-15 20:10:59 +0000

    Added suspension

---
+++ Future-pocalypse/Assets/Scripts/NoSpin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoSpin : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject carMesh;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        float currentPosition = carMesh.transform.localPosition.y;
+        carMesh.transform.localRotation = Quaternion.identity;
+        currentPosition = Mathf.Clamp(currentPosition, -0.6f, 0.9f);
+        carMesh.transform.localPosition = new Vector3(0,currentPosition,0);
+	}
+
+}
