ddc5ac58128b42833d7bfb37da486b7d409989b7
SHyperB <35599114+SHyperB@users.noreply.github.com>
2018-04-24 19:45:52 +0100

    Camera stuff

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 0.125f;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position + offset;
+        Vector3 lerpPosition = Vector3.Lerp(transform.position, targetPosition, speed);
+        transform.position = lerpPosition;
+
+        transform.LookAt(target);
+    }
+}
